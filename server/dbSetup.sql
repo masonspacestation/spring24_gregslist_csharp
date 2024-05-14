2db72742cfa0edf64cd61962b057d6a0076c4129
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-14 10:10:13 -0600

    table setup for cars

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -10,3 +10,65 @@ CREATE TABLE
   ) default charset utf8mb4 COMMENT '';
 
 DROP TABLE accounts;
+
+CREATE TABLE
+  cars (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    make VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year INT UNSIGNED NOT NULL,
+    price INT UNSIGNED NOT NULL,
+    imgUrl VARCHAR(1000) NOT NULL,
+    description VARCHAR(1000),
+    engineType ENUM (
+      "V6",
+      "V8",
+      "V10",
+      "4-Cylinder",
+      "chuncko",
+      "unknown"
+    ),
+    color VARCHAR(255),
+    mileage INT NOT NULL,
+    hasCleanTitle BOOLEAN NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    -- FOREIGN KEY will not let a car be created if the creatorId on the car does not match the id of an account
+    -- ON DELETE CASCADE gets rid of all orphaned data (if an account is deleted, get rid off all the cars that account created)
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+  );
+
+INSERT INTO
+  cars (
+    make,
+    model,
+    year,
+    price,
+    imgUrl,
+    description,
+    engineType,
+    color,
+    mileage,
+    hasCleanTitle,
+    creatorId
+  )
+VALUES
+  (
+    "George",
+    "Jetson",
+    2000,
+    1000000,
+    "https://images.unsplash.com/photo-1598558543997-cd2a5d01c559?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Poot poot",
+    "unknown",
+    "silver",
+    0,
+    true,
+    "66438c07712cf377438dedc3"
+  );
+
+SELECT
+  *
+FROM
+  cars;
